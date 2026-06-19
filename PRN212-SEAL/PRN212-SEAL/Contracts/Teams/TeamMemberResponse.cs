e1fbb817cc829debd3d432de0f9a73edd160af9a
Trần Gia Bảo <giabao991122@gmail.com>
2026-06-20 01:14:40 +0700

    Add team details response contracts

---
+++ PRN212-SEAL/PRN212-SEAL/Contracts/Teams/TeamMemberResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PRN212_SEAL.Contracts.Teams;
+
+public sealed class TeamMemberResponse
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string StudentCode { get; set; } = string.Empty;
+
+    public bool IsLeader { get; set; }
+}
