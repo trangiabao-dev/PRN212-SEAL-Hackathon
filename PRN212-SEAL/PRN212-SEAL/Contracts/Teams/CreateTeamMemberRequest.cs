904a59187fe6550873aee19e61ca3e425ba330c6
Trần Gia Bảo <giabao991122@gmail.com>
2026-06-20 00:35:29 +0700

    u

---
+++ PRN212-SEAL/PRN212-SEAL/Contracts/Teams/CreateTeamMemberRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN212_SEAL.Contracts.Teams
+{
+    public sealed class CreateTeamMemberRequest
+    {
+        public string? Fullname { get; set; }
+        public string? StudentCode { get; set; }
+    }
+}
